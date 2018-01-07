c1ab2f24af16eecc6e08e4a64b22652dd0c6dada
François-Guillaume RIBREAU <github@fgribreau.com>
2018-01-07 14:09:52 +0100

    removed google analytics tracking

--- background.js
+++ background.js
@@ -1,15 +1,3 @@
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-41543039-6']);
-_gaq.push(['_trackPageview']);
-
-
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = 'https://ssl.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
-
-
 var bgSeenBlock = true;
 var bgTypingBlock = true;
 
@@ -24,12 +12,12 @@ chrome.storage.sync.get(['blockSeen', 'blockTyping', 'firstRun'], function(SeenB
 		bgSeenBlock = SeenBlock.blockSeen;
 		bgTypingBlock = SeenBlock.blockTyping;
 	}
-		
+
 	})
 
 
 chrome.webRequest.onBeforeRequest.addListener(
-	function(details) { 
+	function(details) {
 	console.log("Seen attempt");
 		return {cancel: bgSeenBlock
   }
@@ -39,7 +27,7 @@ chrome.webRequest.onBeforeRequest.addListener(
             '*://*.messenger.com/*delivery_receipts*',
             '*://*.facebook.com/*unread_threads*',
             '*://*.messenger.com/*unread_threads*'] }, ['blocking'])
-			
+
 chrome.webRequest.onBeforeRequest.addListener(function(details) {
 	console.log("Typing status: " + bgTypingBlock);
   return {
@@ -47,10 +35,10 @@ chrome.webRequest.onBeforeRequest.addListener(function(details) {
   }
 }, { urls: ['*://*.facebook.com/*typ.php*',
             '*://*.messenger.com/*typ.php*'] }, ['blocking'])
-			
+
 chrome.extension.onRequest.addListener(
   function(request, sender, sendResponse) {
 	console.log("Request recived - SeenBlock = " + request.optBlockSeen + "; TypingBlock = " + request.optBlockTyping);
     bgSeenBlock = request.optBlockSeen;
 	bgTypingBlock = request.optBlockTyping;
-});			
+});
--- options.js
+++ options.js
@@ -1,20 +1,8 @@
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-41543039-6']);
-_gaq.push(['_trackPageview']);
-
-
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = 'https://ssl.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
-
-
 function init() {
 	loadVals();
 	document.getElementById("btnSave").addEventListener("click", save);
-	
-	
+
+
 }
 
 function save() {
@@ -24,17 +12,17 @@ function save() {
 chrome.extension.sendRequest({optBlockSeen: BlockSeen, optBlockTyping: BlockTyping}, function(response) {
   // do something with response.addr...
 });
-	
-	
+
+
 	console.log("Seen is: " + BlockSeen);
 	console.log("Typing is: " + BlockSeen);
-	
+
 	chrome.storage.sync.set({'blockSeen': BlockSeen, 'blockTyping': BlockTyping}, function(){
 	console.log("Synced");
 	});
 
 }
-	   
+
 window.onload = function () {
       init();
 }
